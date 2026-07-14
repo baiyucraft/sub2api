85291493b2f04296a140b45e23abac01d0790e29
baiyucraft <1047852821@qq.com>
2026-07-15 00:09:49 +0800

    fix(deploy): size VM build preflight

--- deploy/release/vm-validate.sh
+++ deploy/release/vm-validate.sh
@@ -73,7 +73,11 @@ server_port=$(extract_config server port)
 [[ $(docker exec sub2api-postgres sh -lc 'psql -X -A -t -U "${POSTGRES_USER:-postgres}" -d sub2api_dev -c "SELECT COUNT(*) FROM schema_migrations WHERE filename='"'"'182_upstream_actual_rate_multiplier.sql'"'"'"') == 0 ]]
 
 free_before=$(df -PB1 /var/lib/docker 2>/dev/null | awk 'NR==2{print $4}' || df -PB1 / | awk 'NR==2{print $4}')
-[[ $free_before -gt 2147483648 ]]
+database_size=$(docker exec sub2api-postgres sh -lc \
+  'psql -X -A -t -U "${POSTGRES_USER:-postgres}" -d postgres -c "SELECT pg_database_size('"'"'sub2api_dev'"'"')"' \
+  | tr -d '\r')
+required_before=$((database_size + 1073741824))
+[[ $free_before -gt $required_before ]]
 export DOCKER_BUILDKIT=1
 mark_stage candidate_build
 docker build --network=host --progress=plain \
@@ -87,9 +91,6 @@ candidate_image_id=$(docker image inspect -f '{{.Id}}' "$tag")
 candidate_size=$(docker image inspect -f '{{.Size}}' "$tag")
 [[ $candidate_image_id =~ ^sha256:[0-9a-f]{64}$ ]]
 free_after_build=$(df -PB1 /var/lib/docker 2>/dev/null | awk 'NR==2{print $4}' || df -PB1 / | awk 'NR==2{print $4}')
-database_size=$(docker exec sub2api-postgres sh -lc \
-  'psql -X -A -t -U "${POSTGRES_USER:-postgres}" -d postgres -c "SELECT pg_database_size('"'"'sub2api_dev'"'"')"' \
-  | tr -d '\r')
 required_free=$((database_size + candidate_size + 536870912))
 [[ $free_after_build -gt $required_free ]]
 probe_suffix=${release_id//[^a-zA-Z0-9]/}
